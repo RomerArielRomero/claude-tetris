2611e418b8d21eef25620efd271dbe6bb7d327db
Romer Romero <romerariel89@gmail.com>
2026-09-24 16:25:04 -0400

    Add game over condition to stop rendering in draw and loop functions

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ const COLORS = {
     '#e57373', // Z - red
     '#90caf9', // J - pale blue
     '#ffb74d', // L - orange
+    '#b0bec5', // N - steel white
   ],
   light: [
     null,
@@ -24,6 +25,7 @@ const COLORS = {
     '#c62828', // Z - red oscuro
     '#1565c0', // J - azul oscuro
     '#e65100', // L - orange oscuro
+    '#546e7a', // N - steel oscuro
   ],
 };
 
@@ -51,6 +53,7 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // N - tuerca hueca
 ];
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
@@ -74,7 +77,7 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const type = Math.floor(Math.random() * 8) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
