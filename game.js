e9ad6f55038e9c1db204dfe81b94380092c8e91c
Romer Romero <romerariel89@gmail.com>
2026-09-24 16:11:51 -0400

    bug corregido

--- game.js
+++ game.js
@@ -221,6 +221,8 @@ function draw() {
     for (let c = 0; c < COLS; c++)
       drawBlock(ctx, c, r, board[r][c], BLOCK);
 
+  if (gameOver) return;
+
   // ghost
   const gy = ghostY();
   for (let r = 0; r < current.shape.length; r++)
@@ -280,6 +282,7 @@ function loop(ts) {
     }
   }
   draw();
+  if (gameOver) return;
   animId = requestAnimationFrame(loop);
 }
 
